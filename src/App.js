46fda91c686dd2284529972d84484e275203045a
unknown <shahrukh1494@gmail.com>
2021-04-20 14:00:41 +0530

    added functionality and css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Review from "./components/Review";
+
 function App() {
-  return <div>React Reviews App</div>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
